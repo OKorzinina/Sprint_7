2840ec9aea20790abd96a8fb52bb2c269134d157
OKorzinina <107882650+OKorzinina@users.noreply.github.com>
2025-11-21 14:33:26 +0100

    Update helps.py

--- helps.py
+++ helps.py
@@ -1,1 +1,33 @@
 
+import requests
+import random
+import string
+from endpoints import CREATE_COURIER
+
+class CourierGenerator:
+    @staticmethod
+    def generate_random_string(length):
+        letters = string.ascii_lowercase
+        return ''.join(random.choice(letters) for i in range(length))
+
+    def register_new_courier_and_return_login_password(self):
+        login_pass_name = []
+
+        login = self.generate_random_string(10)
+        password = self.generate_random_string(10)
+        first_name = self.generate_random_string(10)
+
+        payload = {
+            "login": login,
+            "password": password,
+            "firstName": first_name
+        }
+
+        response = requests.post(CREATE_COURIER, data=payload)
+
+        if response.status_code == 201:
+            login_pass_name.append(login)
+            login_pass_name.append(password)
+            login_pass_name.append(first_name)
+        return {"login": login, "password": password, "firstName": first_name}
+
